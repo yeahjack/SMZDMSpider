9e18af0c2993457913c251463a9e8e1044d6bdf2
Yijie Xu <yxu409@connect.hkust-gz.edu.cn>
2022-10-29 18:00:53 +0800

    creation

---
+++ README.md
@@ -0,0 +1,7 @@
+# Crawler for SMZDM.com
+
+Note:
+
+The webpageCrawler fetches data from each product in each kind and each kind in each brand. Information comes from the webpage.
+
+The jsonCrawler fetches data from the push information of recommendation on the index, which data is JSON.
---
+++ jsonCrawler/multiProcess.py
@@ -0,0 +1,91 @@
+import requests
+import random
+import pandas as pd
+
+# Multiprocessing
+from multiprocessing import Process
+from multiprocessing import Manager
+
+def crawl(s, procnum, return_dict):
+    USER_AGENT_LIST = [
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+]
+    USER_AGENT = random.choice(USER_AGENT_LIST)
+    headers = {'user-agent': USER_AGENT, 'referer': 'https://www.smzdm.com/'}
+    session = requests.Session()
+    session.headers = headers
+    session.proxies = {
+        'http': 'http://127.0.0.1:10809',
+        'https': 'http://127.0.0.1:10809',
+    }
+    results = []
+    for i in range(s, s+10000):
+        url = 'https://www.smzdm.com/homepage/json_more?p=' + str(i) + 'past_num=0'
+        headers = {
+            'user-agent': random.choice(USER_AGENT_LIST), 'referer': 'https://www.smzdm.com/'}
+        r = session.get(url)
+        result = pd.DataFrame.from_dict(r.json()['data'])
+        #result.set_index('article_id', inplace=True)
+        results.append(result)
+        #if i % 100 == 0:
+        #    print(i)
+    return_dict[procnum] = results
+
+
+if __name__ == '__main__':
+    manager = Manager()
+    return_dict = manager.dict()
+    jobs = []
+    for i in range(20):
+        p = Process(target=crawl, args=(i*10000, i, return_dict))
+        jobs.append(p)
+        p.start()
+    for proc in jobs:
+        proc.join()
+    return_dict = [pd.concat(return_dict[i]) for i in range(len(return_dict))]
+    #results = pd.concat(return_dict)
+    df_results = pd.concat(return_dict)
+    '''
+    df_results = df_results.drop(columns=['article_content', 'article_type', 'article_link_list', 'article_pic_style', 'article_item_title_mode', 'article_stock_note', 'article_link_name', 'article_channel_url', 'article_channel_class', 'article_channel_id', 'article_channel_note', 'article_mall_icon', 'binded_biaodan', 'article_country_icon', 'binded_biaodan', 'cid', 'atp', 'tag_id', 'wxapp', 'matches_rules', 'state_type', 'sort_type', 'data_from_type',
+                             'abtoken', 'article_hash_id', 'article_user_url', 'article_referrals_avatar', 'anonymous', 'official_auth_icon', 'official_auth_desc', 'article_real_date', 'article_love_rating', 'article_url_wap', 'article_show_tags', 'feed_tab', 'mall_more_info', 'article_title_height_light', 'blog_rzlx_name', 'buy_btn_domain', 'pic_url_dis', 'pic_url_dis_wh', 'news_direct_link', 'news_direct_link_arr', 'article_link_type', 'display_none'], axis=1)
+    '''
+    df_results = df_results.drop(
+    df_results[pd.isna(df_results['article_price'])].index)
+    
+    df_results.to_csv('test_10000.csv', encoding='utf_8_sig', mode='a')
+
+    
